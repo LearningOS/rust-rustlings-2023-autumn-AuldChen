556b427ef5253c286d5b163f7fbade9e190e0f3c
AuldChen <2990566809@qq.com>
2023-10-11 23:18:31 +0800

    update

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -11,7 +11,7 @@
 fn slice_out_of_array() {
     let a: [i32; 5] = [1, 2, 3, 4, 5];
 
-    let nice_slice:&[i32] = &a[1..4];
+    let nice_slice: &[i32] = &a[1..4];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
